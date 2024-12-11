ede831d3ae0a4b72366d8fa1334498cc18c71435
sjlee <sj_lee@yahoo.com>
2024-12-11 03:58:03 -0500

    MAX_PLY = 224, MAX_MOVES = 224, and same for TB_MAX_MOVES and TB_MAX_PLY.

--- src/types.h
+++ src/types.h
@@ -30,7 +30,7 @@ enum { WHITE, BLACK };
 
 enum { PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING };
 
-enum { MAX_PLY = 192, MAX_MOVES = 192 };
+enum { MAX_PLY = 224, MAX_MOVES = 224 };
 
 enum {
     WHITE_PAWN   =  0, BLACK_PAWN   =  1,
